83d5a09c146fae9a5c1d0bd28a76d6e4e28a6399
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2020-10-14 03:15:42 -0600

    conversions such as lb to kg & miles to kilometers.

---
+++ conversions-pounds-to-kilo.php
@@ -0,0 +1,28 @@
+<?php
+// number in pounds we want to convert to kilograms
+$pounds = 180;
+// floating point value for the pound to kilogram conversion
+$lb_to_kg = 0.453592;
+// use the variables above to calculate pounds multiplied by the kilogram conversion
+$kilograms = $pounds * $lb_to_kg;
+// display the pounds to kilograms
+echo "Weight: ";
+echo $pounds;
+echo " lb = ";
+echo $kilograms;
+echo "kg";
+
+
+// number in miles we want to convert to kilometers
+$miles = 2.5;
+// floating point value for the miles to kilometer conversion
+$mile_to_km = 1.60934;
+// use variables above to calucalte the miles multiplied by the kilometer conversion
+$kilometers = $miles *$mile_to_km;
+//display the miles to kilometers
+echo "Distance: ";
+echo $miles;
+echo " miles - ";
+echo $kilometers;
+echo " km";
+?>
