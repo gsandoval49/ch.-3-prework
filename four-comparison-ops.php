79aa0cc48328a380de6ec776e4c1704f5b123b32
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2020-12-03 23:49:13 -0700

    comparison operators refresher

---
+++ four-comparison-ops.php
@@ -0,0 +1,12 @@
+<?php
+$a = 2; $b = 3;
+
+if ($a < $b) echo "$a is less than $b<br>";
+if ($a > $b) echo "$a is greater than $b<br>";
+if ($a <= $b) echo "$a is less than or equal to $b<br>";
+if ($a >= $b) echo "$a is greater than or equal to $b<br>";
+
+// outputs 2 is less than 3
+// 2 is less than or equal to 3
+
+?>
