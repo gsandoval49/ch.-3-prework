a3e329d3d6feca5f41d80d60f0dfaf449014103d
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2020-12-03 23:52:49 -0700

    addt'l notes

--- four-comparison-ops.php
+++ four-comparison-ops.php
@@ -9,4 +9,8 @@ if ($a >= $b) echo "$a is greater than or equal to $b<br>";
 // outputs 2 is less than 3
 // 2 is less than or equal to 3
 
+// try setting them to the same value...
+// scenario where $a = 3 and $b = 3
+// then equal to arguments would be the outputs.
+
 ?>
