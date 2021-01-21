348efa4204f2d251f9d005b143c412a8a4808196
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2021-01-21 12:05:28 -0700

    else if template for month end balance in checking to put toward your savings and slush fund. class ex.

---
+++ elseif-bank-savings-slush.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * ex. of personal savings and using a slush fund
+ * after your month end balance in checking.
+ *
+ * slush fund ex. upcoming snowboard trip
+ */
+
+// enter your balances here
+$balance = 2500;
+$savings = 6000;
+$slush   = 100;
+
+if ($balance < 1000) {
+  echo "work on where you can, save more & spend less";
+} elseif ($balance < 1500) {
+  echo "good work, but room for improvement" . "<br>";
+  // amount of 10% of balance and deposit into savings
+  $depositSavings = $balance * .10;
+  echo "deposit amt into savings: $depositSavings" . "<br>";
+
+  // this is new savings amount after deposit
+  $newSavings = $savings + $depositSavings;
+  echo "new savings balance SB: $newSavings" . "<br>";
+} else {
+  echo "great job! increase savings & slush fund this month" . "<br>";
+  // 10% of balance into savings
+  $depositSavings = $balance * .10;
+  echo "deposit amt into savings: $depositSavings" . "<br>";
+  // this is new savings amount after deposit
+  $newSavings = $savings + $depositSavings;
+  echo "new savings balance SB: $newSavings" . "<br>";
+  // deposit 5% into slush fund
+  $depositSlush = $balance * .05;
+  echo "deposit amt into slush fund: $depositSlush" . "<br>";
+  $newSlush = $slush + $depositSlush;
+  echo "new slush fund amt: $newSlush" . "<br>";
+}
+
+?>
